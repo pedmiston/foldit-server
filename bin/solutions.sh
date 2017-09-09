b20def80fcf8f28ff68e0e6faa25e4de31301559
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-09-09 17:00:16 -0500

    Cleaning up

--- bin/solutions.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-source ~/.profile
-~/foldit/playbooks/scripts/select-new-puzzle.py ~/foldit/playbooks/puzzle-info.csv > new-puzzle-id && \
-puzzle_id=`cat new-puzzle-id` && \
-ansible-playbook ~/foldit/playbooks/get_solutions.yml -e puzzle_id=${puzzle_id}
