7063d62455bcd77b60b86cac7086d252ede2dc64
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-09-29 06:12:30 -0500

    Better cleanup after scrape is complete

--- bin/scrape.py
+++ bin/scrape.py
@@ -9,12 +9,13 @@ cmdf = 'ansible-playbook {name}.yml {arg_string}'
 errorf = 'error in playbook: {cmd}\n{stdout}\n'
 
 def run(name, arg_string=''):
+    print('running playbook: {} {}'.format(name, arg_string), flush=True)
     cmd = cmdf.format(name=name, arg_string=arg_string)
     try:
         subprocess.check_output(cmd.split(), stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as err:
         # convert bytes to string so that new lines are printed
-        output_str = str(err.output)
+        output_str = err.output.decode('utf-8')
         sys.stderr.write(errorf.format(cmd=err.cmd, stdout=output_str))
 
 if __name__ == '__main__':
@@ -26,36 +27,27 @@ if __name__ == '__main__':
     parser.add_argument('--dont-update-scraper', action='store_true')
     args = parser.parse_args()
 
-    print('running playbook: prepare', flush=True)
     run('prepare')
 
     # Find available solutions unless told not to
     if not args.skip_find:
-        print('running playbook: find', flush=True)
         run('find')
         run('workload')
 
     # Install/update scraper unless told not to
     if not args.dont_update_scraper:
-        print('running playbook: install', flush=True)
         run('install')
 
     workload_dir = Path(args.workload_dir)
     for i, workload in enumerate(workload_dir.iterdir()):
-        # Skip any file in the workload dir that has a file extension
+        # Skip files that have a file extension
         if workload.suffix != '':
             continue
 
-        print('running playbook: scrape workload #{} {}'.format(i, workload.name),
-              flush=True)
         run('scrape', arg_string='-e workload={}'.format(workload.name))
 
         if i%args.batches_per_push == args.batches_per_push-1:
-            print('running playbook: push', flush=True)
             run('push')
 
-    print('running playbook: push', flush=True)
     run('push')
-
-    print('cleaning up')
-    subprocess.call('rm data/available/*.txt'.split())
+    run('cleanup')
