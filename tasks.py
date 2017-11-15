a65ea3aa20ff17111805c91a314eb866a1cdf088
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-11-15 10:45:12 -0600

    Remove refs to digital ocean

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import csv
+from pathlib import Path
 from os import environ
 import boto3
 import botocore
@@ -12,22 +13,27 @@ BUCKET = 'foldit'
 @task
 def list_keys(ctx):
     """List all keys in S3 bucket."""
-    client = new_s3_session()
-    resp = client.list_objects_v2(Bucket=BUCKET)
-    keys = [obj['Key'] for obj in resp['Contents']]
+    session = new_s3_session()
+    keys = _list_keys(session)
     print('\n'.join(keys))
 
 @task
 def get_key(ctx, key):
     """Download key from S3 bucket."""
-    client = new_s3_session()
-    try:
-        client.download_file(BUCKET, key, key)
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == "404":
-            print(f'Object "{key}" does not exist in bucket "{BUCKET}"')
-        else:
-            raise
+    session = new_s3_session()
+    _get_key(key, session)
+
+@task
+def get_all_keys(ctx, dst):
+    session = new_s3_session()
+    keys = _list_keys(session)
+
+    dst = Path(dst)
+    if not dst.is_dir():
+        dst.mkdir()
+
+    for key in keys:
+        _get_key(key, session, dst)
 
 def new_s3_session():
     session = boto3.session.Session()
@@ -35,3 +41,22 @@ def new_s3_session():
                             aws_access_key_id=environ['AWS_ACCESS_KEY'],
                             aws_secret_access_key=environ['AWS_SECRET_KEY'])
     return client
+
+def _list_keys(session):
+    resp = session.list_objects_v2(Bucket=BUCKET)
+    keys = [obj['Key'] for obj in resp['Contents']]
+    return keys
+
+def _get_key(key, session, dst=None):
+    if dst is not None:
+        dst = Path(dst, key)
+    else:
+        dst = key
+
+    try:
+        client.download_file(BUCKET, key, dst)
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print(f'Object "{key}" does not exist in bucket "{BUCKET}"')
+        else:
+            raise
